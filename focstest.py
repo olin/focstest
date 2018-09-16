412c8659362e89aca7457fb017e3cb5e0af7defb
Evan Lloyd New-Schmidt <evan@new-schmidt.com>
2018-09-15 23:03:54 -0400

    Add command-line interface

--- focstest.py
+++ focstest.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 import re
@@ -9,10 +10,10 @@ from bs4 import BeautifulSoup
 import requests
 
 
-logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(name=__name__)  # create logger in order to change level later
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 
-FILE = 'homework1.ml'
-URL = "http://rpucella.net/courses/focs-fa18/homeworks/h1.html"
 
 # selectors for parsing html
 CODE_BLOCK_SELECTOR = 'div.code pre'  # css selector to get code blocks
@@ -35,7 +36,7 @@ def get_blocks(html):
     page = BeautifulSoup(html, 'html.parser')
     code_blocks = page.select(CODE_BLOCK_SELECTOR)
     if len(code_blocks) == 0:
-        logging.error('Code block selector {!r} returned no matches'.format(
+        logger.error('Code block selector {!r} returned no matches'.format(
             CODE_BLOCK_SELECTOR))
     return [block.get_text() for block in code_blocks]
 
@@ -47,7 +48,7 @@ def get_tests(text):
     """
     tests = TEST_COMP.findall(text)
     if len(tests) == 0:
-        logging.error('Test/response pattern {!r} returned no matches'.format(
+        logger.error('Test/response pattern {!r} returned no matches'.format(
             TEST_PATTERN))
     return tests
 
@@ -68,7 +69,7 @@ def _run_ocaml_code(code):
         except subprocess.TimeoutExpired:
             p.kill()
             outs, errs = p.communicate()
-            logging.warning('Ocaml process timed out: {} {}'.format(outs, errs))
+            logger.warning('Ocaml process timed out: {} {}'.format(outs, errs))
     return (outs, errs)
 
 
@@ -90,20 +91,38 @@ def run_test(code: str, expected_out: str, file: str = None):
     outs, errs = _run_ocaml_code(command)
     matches = OCAML_COMP.findall(outs)
     if len(matches) != 3 and file is not None:
-        logging.error("Expected 2 matches, got {}".format(len(matches)))
+        logger.error("Expected 2 matches, got {}".format(len(matches)))
     elif len(matches) != 2 and file is None:
-        logging.error("Expected 1 , got {}".format(len(matches)))
+        logger.error("Expected 1 , got {}".format(len(matches)))
     else:
         output = matches[-2]  # don't use empty final match from #quit;;
         return (output == expected_out, output)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run ocaml "doctests".')
+    input_types = parser.add_mutually_exclusive_group(required=True)
+    input_types.add_argument('-u', '--url', type=str,
+                        help='the url to scrape tests from')
+    parser.add_argument('ocaml-file', type=str,
+                        help='the ocaml file to test against')
+    # parser.add_argument('-v', '--verbose', action='store_true',
+                    # help='increase test output verbosity')
+    parser.add_argument('--log', choices=['debug','info','warning'],
+                        help='the program log level')
+    args = parser.parse_args()
+    if args.log:
+        numeric_level = getattr(logging, args.log.upper(), None)
+        logger.setLevel(numeric_level)
+
+    URL = args.url
+    FILE = getattr(args, 'ocaml-file')
+
     # get and cache webpage
     CACHE_DIR = 'focstest-cache/'
     if not os.path.exists(CACHE_DIR):
         os.makedirs(CACHE_DIR)
-        logging.info('Created cache directory at {!r}'.format(CACHE_DIR))
+        logger.info('Created cache directory at {!r}'.format(CACHE_DIR))
     page_name = os.path.basename(urlparse(URL).path)  # get page name from url
     html_filepath = os.path.join(CACHE_DIR, page_name)  # local filepath
 
@@ -111,7 +130,7 @@ if __name__ == "__main__":
     if not os.path.isfile(html_filepath):
         response = requests.get(URL)
         if response.status_code != 200:  # break if webpage can't be fetched
-            logging.critical("Unable to fetch url {}: Status {}: {}".format(
+            logger.critical("Unable to fetch url {}: Status {}: {}".format(
                 URL,
                 response.status_code,
                 response.reason))
@@ -120,9 +139,9 @@ if __name__ == "__main__":
         html = response.text
         with open(html_filepath, 'w') as htmlcache:
             htmlcache.write(html)
-            logging.debug("Saved {!r} to cache at {!r}".format(URL, html_filepath))
+            logger.debug("Saved {!r} to cache at {!r}".format(URL, html_filepath))
     else:
-        logging.debug("Found cached version at {!r}".format(html_filepath))
+        logger.debug("Found cached version at {!r}".format(html_filepath))
         with open(html_filepath, 'r') as htmlcache:
             html = htmlcache.read()
 
@@ -131,13 +150,17 @@ if __name__ == "__main__":
     blocks = get_blocks(html)
     # parse code blocks for tests
     test_suites = [get_tests(block) for block in blocks]
-    logging.info("Found {} test suites and {} tests total".format(
+    logger.info("Found {} test suites and {} tests total".format(
         len(test_suites),
         sum([len(suite) for suite in test_suites])))
+        num_tests))
     # TODO: save tests to file
 
     # run tests
     # TODO: check for "not implemented" error
+    if not os.path.exists(FILE):
+        logger.critical("File {} does not exist".format(FILE))
+        sys.exit(1)
     for suite in test_suites:
         for test, expected_output in suite:
             result, output = run_test(test, expected_output, file=FILE)
